cdf635e2ced68f4bef1914852410cdb79aec1f1b
Gabor Szabo <gabor@szabgab.com>
2015-03-29 09:23:28 +0300

    list recent

--- app/metacpan.js
+++ app/metacpan.js
@@ -1,6 +1,13 @@
 var metacpan = {}
-metacpan.search = function(query, callback) {
+metacpan.recent = function(count, callback) {
+	metacpan.get('http://api.metacpan.org/v0/release/_search?q=status:latest&fields=distribution,name,status,date,abstract&sort=date:desc&size=' + count, count, callback);
+};
 
+metacpan.release = function(query, callback) {
+	metacpan.get("http://api.metacpan.org/v0/release/" + query, query, callback);
+};
+
+metacpan.get = function(url, query, callback) {
     xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
@@ -10,7 +17,7 @@ metacpan.search = function(query, callback) {
         }
     }
 
-    xmlhttp.open("GET", "http://api.metacpan.org/v0/release/" + query, true);
+    xmlhttp.open("GET", url, true);
     //setTimeout(function() {  xmlhttp.abort()  },40000);
     xmlhttp.send();
 };
