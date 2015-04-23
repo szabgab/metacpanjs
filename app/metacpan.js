0c7f848eb9017a67e6c2b682ebeb8fcb4276fe21
Gabor Szabo <gabor@szabgab.com>
2015-04-23 10:12:51 +0200

    add conditional helper

--- app/metacpan.js
+++ app/metacpan.js
@@ -26,6 +26,32 @@ metacpan.get = function(url, query, callback) {
 	xmlhttp.send();
 };
 
+Handlebars.registerHelper('iff', function(a, operator, b, opts) {
+    var bool = false;
+    switch(operator) {
+       case '==':
+           bool = a == b;
+           break;
+       case '!=':
+           bool = a != b;
+           break;
+       case '>':
+           bool = a > b;
+           break;
+       case '<':
+           bool = a < b;
+           break;
+       default:
+           throw "Unknown operator " + operator;
+    }
+ 
+    if (bool) {
+        return opts.fn(this);
+    } else {
+        return opts.inverse(this);
+    }
+});
+
 
 function search() {
 	var query = document.getElementById('query').value;
