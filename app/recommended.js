0c4078d93bc64cbe82281d417115dfadb5bb1c19
Gabor Szabo <gabor@szabgab.com>
2015-05-04 12:21:01 +0300

    more recommendations

--- app/recommended.js
+++ app/recommended.js
@@ -1,6 +1,9 @@
 metacpan.recommended = {
 	'async'           : [ 'POE', 'AnyEvent', 'IO::Async' ],
 	'csv'             : [ 'Text::CSV', 'Spreadsheet::Read' ],
+	'config'          : [ 'Config::Tiny', 'Config::General', 'Config::Any' ],
+	'Dancer'          : [ 'Dancer', 'Dancer::Plugin::DBIC', 'Dancer::Plugin::Auth::Extensible', 'Dancer::Plugin::Passphrase', 'Dancer::Plugin::Email', 'Dancer::Plugin::Lexicon', 'Dancer::Plugin::Auth::Extensible::Provider::Usergroup' ],
+	'Dancer2'         : [ 'Dancer2::Plugin::Path::Class', 'Dancer2::Plugin::DBIC', 'Dancer2::Plugin::Ajax', 'Dancer2::Plugin::Passphrase', 'Dancer2::Plugin::Auth::Extensible'],
 	'database'        : [ 'DBI', 'DBIx::Class', 'DBD::mysql', 'DBD::SQLite', 'MongoDB' ],
 	'date-time'       : [ 'DateTime', 'DateTime::Tiny' ],
 	'email'           : [ 'Email::Valid', 'Email::Stuffer', 'Email::Sender' ],
@@ -10,9 +13,10 @@ metacpan.recommended = {
 	'logging'         : [ 'Log::Log4perl', 'Log::Dispatch' ],
 	'oop'             : [ 'Moose', 'Moo' ],
 	'perl'            : [ 'Devel::NYTProf', 'Modern::Perl', 'Perl::Critic', 'Perl::Tidy', 'Module::Version', 'App::cpanminus', 'App::cpanoutdated', 'App::perlbrew', 'App::FatPacker', 'Carton', 'Pinto', 'local::lib' ],
-	'testing'         : [ 'Test::More', 'Test::Most', 'Devel::Cover' ],
+	'testing'         : [ 'Test::More', 'Test::Most', 'Devel::Cover', 'Test::Differences', 'Test::Code::TidyAll', 'Test::Mock::Simple', 'Test::MockTime', 'Test::Perl::Critic', 'Test::Version', 'Test::Warn', 'Test::WWW::Mechanize' ],
 	'xml'             : [ 'XML::LibXML', 'XML::LibXSLT', 'XML::SAX', 'XML::Twig' ],
 	'web-crawling'    : [ 'WWW::Mechanize', 'LWP::UserAgent', 'LWP::Simple', 'WWW::Selenium' ],
-	'web-development' : [ 'Dancer2', 'Mojolicious', 'Plack', 'Catalyst', 'Template']
+	'web-development' : [ 'Dancer2', 'Mojolicious', 'Plack', 'Catalyst', 'Template'],
+	'web-server'      : [ 'Starman', 'Twiggy', 'PSGI' ]
 };
 
